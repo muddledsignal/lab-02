720962c2db010382e5c387d6ed4c2d20a47c4ce7
Suzanne Leber <37949595+muddledsignal@users.noreply.github.com>
2018-10-07 00:04:35 -0700

    Revert "Lab unfinished, stopping for the night"

--- list/index.js
+++ list/index.js
@@ -5,24 +5,4 @@ const List = require('./list-constructor.js');
 let stuff = new List();
 stuff.push('a');
 stuff.push('b');
-// let last = stuff.pop();
-
-console.log(stuff);
-
-// stuff.forEach((value, idx) => {
-//   console.log(value, idx);
-// });
-//
-// stuff.forEach((value, idx) => {
-//   // if(value == 'a') {
-//   //   console.log('Yay!', value, idx);
-//   // }
-// });
-
-
-// console.log(stuff.data[0]);
-// stuff.for()
-
-
-// stuff.forEach();
-// console.log(List);
+let last = stuff.pop();
